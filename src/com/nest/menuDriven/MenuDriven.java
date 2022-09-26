ab705615e23a3818d6bd17b9531471edf3cc27ad
Akshayjosephtomy <akshayjt007@gmail.com>
2022-09-26 15:17:28 +0530

    switch added

---
+++ src/com/nest/menuDriven/MenuDriven.java
@@ -0,0 +1,39 @@
+package com.nest.menuDriven;
+
+import java.util.Scanner;
+
+public class MenuDriven {
+    public static void main(String[] args) {
+        int choice;
+        while (true)
+        {
+            System.out.println("Select an option");
+            System.out.println("1.  Add student");
+            System.out.println("2. Search student");
+            System.out.println("3. Delete student");
+            System.out.println("4. View all student");
+            System.out.println("5. Exit");
+
+            Scanner scanner=new Scanner(System.in);
+            choice=scanner.nextInt();
+
+            switch (choice)
+            {
+                case 1:
+                    System.out.println("Add student selected");
+                    break;
+                case 2:
+                    System.out.println("Select student selected");
+                    break;
+                case 3:
+                    System.out.println("Delete student selected");
+                    break;
+                case 4:
+                    System.out.println("view all students selected");
+                    break;
+                case 5:
+                    System.exit(0);
+            }
+        }
+    }
+}
