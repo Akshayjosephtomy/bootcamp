a4eaf3d3b7c0bb3f3fc78ff8c9c09f01aba2edd3
Akshay Joseph Tomy <86040610+Akshayjosephtomy@users.noreply.github.com>
2022-09-27 12:29:24 +0530

    Update Calculator.java

--- src/com/nest/calculator/Calculator.java
+++ src/com/nest/calculator/Calculator.java
@@ -33,8 +33,16 @@ public class Calculator {
                     System.out.println(a*b);
                     break;
                 case 4:
-                    System.out.println(a/b);
-                case 5:System.exit(0);
+                    try {
+                        System.out.println(a / b);
+                    }
+                    catch (ArithmeticException ae) {
+                        System.out.println("Division by zero is not possible");
+
+                    }
+                    break;
+                case 5:
+                    System.exit(0);
             }
         }
     }
